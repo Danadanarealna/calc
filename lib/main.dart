71d38bc191a17cf642f21889f009ae105efef0d2
Najam Pradana Mukti <Danadana123spijiso@gmail.com>
2025-03-19 05:37:45 +0700

    Added Login

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
-
-import 'package:flutter_application_1/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/login.dart'; // Keep only necessary imports
 
 void main() {
   runApp(const MyApp());
@@ -9,7 +8,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -18,8 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
-      home: const MyCalculator(title: 'Calculator'),
+      home: const MyLoginPage(), // Start with login page
     );
   }
 }
-
